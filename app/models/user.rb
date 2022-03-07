af84d150e1342d7bacb6fb0e9064e30c9ff8b18b
Denis Naughton <mjolinor75@hotmail.com>
2022-03-06 22:42:55 -0500

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
-    self.role ||= :student
+    self.role ||= :districtadmin
   end
 
   devise :database_authenticatable, :registerable,
