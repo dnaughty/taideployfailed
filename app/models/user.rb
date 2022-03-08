700d7a425b8ee4f088ba399053e17c7bc46fb501
Denis Naughton <mjolinor75@hotmail.com>
2022-03-08 18:03:53 -0500

    random styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   enum role: { superadmin: 0, districtadmin: 1, schooladmin: 2, teacher: 3, student: 4 }
-  after_initialize :set_default_role, :if => :new_record?
+  # after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= :student
-  end
+  # def set_default_role
+  #   self.role ||= :student
+  # end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
